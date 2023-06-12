5ad3d7d76dd0d04115c46daea599d7bbd542582f
Cong Hoang <jackhoang2411@gmail.com>
2023-06-12 16:05:03 -0700

    create welcome screen with localization feature

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,34 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+// Static method to access the _MyAppState from other widgets
+  static _MyAppState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale? _locale;
+
+// Method to set the selected locale
+  void setLocale(Locale value) {
+    setState(() {
+      _locale = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      locale: _locale,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: MyHomePage(
+      home: const MyHomePage(
         title: "Welcome",
       ),
     );
@@ -22,7 +40,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -35,7 +53,35 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: Center(
-        child: Text(AppLocalizations.of(context)!.helloUser),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              AppLocalizations.of(context)!.helloUser,
+              style: const TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            DropdownButton<String>(
+              value: AppLocalizations.of(context)!.localeName,
+              onChanged: (value) {
+                // Accessing the setLocale method from MyApp using the static of() method
+                MyApp.of(context)!
+                    .setLocale(Locale.fromSubtags(languageCode: value!));
+              },
+              items: [
+                DropdownMenuItem(
+                  value: 'en',
+                  child: Text(AppLocalizations.of(context)!.english),
+                ),
+                DropdownMenuItem(
+                  value: 'es',
+                  child: Text(AppLocalizations.of(context)!.spanish),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
